09bc8ba1435f1249599503093298222a969184ee
marimar <mariadelmarsanchez802@gmail.com>
2025-04-02 12:33:58 -0500

    proeycto inicial

---
+++ src/components/Clientes/IncioCli.jsx
@@ -0,0 +1,12 @@
+import Menu from "../Menu";
+
+function InicioCli() {
+    return(
+        <>
+        <Menu></Menu>
+        <h1>Contenido del cliente</h1>
+        </>
+    );
+}
+
+export default InicioCli;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,9 @@
+function Menu() {
+    return(
+        <nav>
+            <h1>Mi menu</h1>
+        </nav>
+    )
+    /*comentarios*/
+}
+export default Menu;
