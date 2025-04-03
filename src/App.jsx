46301c1c68210df51ea785383e6f43649fc06225
mrTinto <2030suarezmolano@gmail.com>
2025-04-03 13:51:23 -0500

    segundoCambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
-import InicioCli from './components/Clientes/IncioCli';
-import InicioTienda from './components/Tienda/InicioTienda';
+import InicioSe from './components/iniciosesion/inicioSe';
+import HomeCli from './components/Clientes/HomeCli';
+
 
 function App() {
   const [count, setCount] = useState(0)
     return(
-       <Routes>
-        <Route path='/' element={<InicioCli></InicioCli>}></Route>
-        <Route path='/tienda' element={<InicioTienda></InicioTienda>}></Route>
-       </Routes> 
-        
+      <Routes>
+        <Route path='/login' element={<InicioSe></InicioSe>}></Route>
+        <Route path='/' element={<HomeCli></HomeCli>}></Route>
+      </Routes>
     );
     
 }
