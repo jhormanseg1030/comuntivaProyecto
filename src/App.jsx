4a2a8fdea47041663996ef5e4840337042256b53
jhorman segura <jhormansegura2012@gmail.com>
2025-04-17 20:54:35 -0500

    rama9Lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
-import InicioSe from './components/InicioSesion/InicioSe';
 import HomeCli from './components/Clientes/HomeCli';
 import HomeTienda from "./components/Tienda/HomeTienda";
 import SegPrinci from './components/SegPrincipal/SegPrinci';
+import InicioSe from './components/iniciosesion/InicioSe';
+import RegistrarUsu from './components/Registrar/RegistrarUsu';
 
 
 function App() {
   const [count, setCount] = useState(0)
     return(
       <Routes>
-        <Route path='/login' element={<InicioSe></InicioSe>}></Route>
+        <Route path='/Login' element={<InicioSe></InicioSe>}></Route>
         <Route path='/' element={<HomeCli></HomeCli>}></Route>
         <Route path='/Tienda' element={<HomeTienda></HomeTienda>}></Route>
         <Route path='/Segunda' element={<SegPrinci></SegPrinci>}></Route>
+        <Route path='/Registro' element={<RegistrarUsu></RegistrarUsu>}></Route>
       </Routes>
     );
 
--- src/components/iniciosesion/InicioSe.jsx
+++ src/components/iniciosesion/InicioSe.jsx
@@ -1,29 +1,26 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import './estilos.css';
-import '../imagenes/Conmutiva.jpeg';
-function InicioSe() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Correo Electronico</Form.Label>
-        <Form.Control type="email" placeholder="Ingrese email" />
-        <Form.Text className="text-muted">
-        Nunca compartiremos tu correo electrónico con nadie más..
-        </Form.Text>
-      </Form.Group>
+import React from 'react';
+import './estilos.css'
+import { Link } from 'react-router-dom';
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Ingrese
-      </Button>
-    </Form>
+const InicioSe = () => {
+  return (
+      <div className=" Container3">
+            <form className="form1">
+                <h2>Inicio de sesion</h2>
+                <div className="form-group1">
+                    <label htmlFor="name">Usuario o Correo Electronico</label><br/>
+                    <input type="text" id="name" required />
+                </div>
+
+                <div className="form-group1">
+                    <label htmlFor="password">Contraseña</label><br />
+                    <input type="password" id="password" required />
+                    <Link to="/Registro">¿Olvido su contraseña?</Link>
+                </div>
+
+                <button type="submit" className="submit-button1 ">Enviar</button>
+            </form>
+        </div>
   );
 }
 
