2e5ef66d9964c804cfd83ff5b67174f1b505048a
EivorKinKest <jmqo2015@gmail.com>
2025-04-21 13:43:35 -0500

    modificaciones

--- src/components/Tienda/ConfTienda.jsx
+++ src/components/Tienda/ConfTienda.jsx
@@ -1,6 +1,7 @@
 // src/components/ConfTienda.js
 import React, { useState } from 'react';
 import './ConfTienda.css';
+import logo from '../imagenes/logo.jpg';
 
 const ConfTienda = () => {
   const [contenido, setContenido] = useState('Inicio');
@@ -13,8 +14,8 @@ const ConfTienda = () => {
     <div className="conf-tienda">
       <nav className="menu">
         <div className="logo">
-          <h1>COMUCTIVA</h1>
-          <button>Ver tienda</button>
+        <img src={logo} alt="Logo" className="nav-brand"/>
+        <button>Ver tienda</button>
         </div>
         <ul className="menu-principal">
           <li onClick={() => manejarSeleccion('Inicio')} className={contenido === 'Inicio' ? 'active' : ''}>Inicio</li>
@@ -22,13 +23,13 @@ const ConfTienda = () => {
           <li onClick={() => manejarSeleccion('Productos')} className={contenido === 'Productos' ? 'active' : ''}>Productos</li>
           <li onClick={() => manejarSeleccion('Historial')} className={contenido === 'Historial' ? 'active' : ''}>Historial</li>
           <li onClick={() => manejarSeleccion('Promociones')} className={contenido === 'Promociones' ? 'active' : ''}>Promociones</li>
-        </ul>
+      
         <h2>Personalización</h2>
-        <ul>
+    
           <li onClick={() => manejarSeleccion('Galería')} className={contenido === 'Galería' ? 'active' : ''}>Galería</li>
-        </ul>
+    
         <h2>Configuración</h2>
-        <ul>
+
           <li onClick={() => manejarSeleccion('General')} className={contenido === 'General' ? 'active' : ''}>General</li>
           <li onClick={() => manejarSeleccion('Proceso de compra')} className={contenido === 'Proceso de compra' ? 'active' : ''}>Proceso de compra</li>
           <li onClick={() => manejarSeleccion('Cuenta')} className={contenido === 'Cuenta' ? 'active' : ''}>Cuenta</li>
@@ -37,19 +38,35 @@ const ConfTienda = () => {
       <div className="contenido">
         <h1>{contenido}</h1>
         <div className="contenido-dinamico">
-          {contenido === 'Inicio' && <p>Aquí puedes ver las tareas para configurar tu tienda.</p>}
+          {contenido === 'Inicio' && <div><h2>Gestionar descuentos y promociones</h2>
+          <p>Aquí puedes ver las tareas para configurar tu tienda.</p>
+          <button>hjsjdks</button></div>}
           {contenido === 'Pedidos' && <p>Aquí puedes gestionar tus pedidos.</p>}
-          {contenido === 'Productos' && <p>Aquí puedes agregar y gestionar tus productos.</p>}
+          {contenido === 'Productos'&& 
+          <div>
+          <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+</svg>Añadir Producto</button></div>}
           {contenido === 'Historial' && <p>Aquí puedes ver el historial de tus transacciones.</p>}
-          {contenido === 'Promociones' && <p>Aquí puedes gestionar tus promociones.</p>}
+
+          {contenido === 'Promociones' && 
+          <div>
+            <h2>Gestionar descuentos y promociones</h2>
+            <p>Aquí puedes gestionar tus promociones, añade códigos de descuento y descuentos automáticos que se aplican en el pago</p>
+            <button>Crear Promociones</button>
+            </div>}
+
           {contenido === 'Galería' && <p>Aquí puedes gestionar la galería de tu tienda.</p>}
           {contenido === 'General' && <p>Aquí puedes ajustar la configuración general de tu tienda.</p>}
           {contenido === 'Proceso de compra' && <p>Aquí puedes configurar el proceso de compra.</p>}
           {contenido === 'Cuenta' && <p>Aquí puedes gestionar la información de tu cuenta.</p>}
         </div>
         <div className="ayuda">
-          <h2>¿Necesitas Ayuda?</h2>
+          <h2><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+</svg></h2>
           <p>Aquí puedes encontrar orientación sobre cómo usar la plataforma.</p>
+          <button>¿Necesitas Ayuda? </button>
         </div>
       </div>
     </div>
