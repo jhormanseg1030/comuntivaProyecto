59c38343d0f3357ffe513d41e21ae6b02bf637a1
jhorman <jhormansegura2012@gmail.com>
2025-06-27 13:21:48 -0500

    Arreglo_Pago

--- src/components/FinalDePago/Pago.jsx
+++ src/components/FinalDePago/Pago.jsx
@@ -1,9 +1,12 @@
+
+
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import banano from '../imagenes/banano.jpg';
 import logo from '../Imagenes/logo.jpg';
 import './Pago.css';
-const Pago = () => {
+function Pago () {
     return (
         <div>
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -20,10 +23,36 @@ const Pago = () => {
         </div>
         <div className='Pag2'>
             <h2>Metodo de pago</h2>
-            <h5>Agregar</h5>
+            <h5>Agrega te metodo de pago</h5>
+            <input type="checkbox"/>
+        </div>
+        <div className=' Pag3'>
+            <h2>Nombre de la tienda</h2>
+                <div className='Cami'>
+                    <img src={banano} alt="banano" />
+                <div>
+                    <h5 className='text'>Descripcion del producto</h5>
+                    <h5>Valor del producto</h5>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h2>Confirmacion de pedido</h2>
+            </div>
+            <div>
+                <h5>subtotal</h5>
+                <h5>Envio:</h5>
+                <h5>Fecha de entrega</h5>
+                <h5>Total a pagar</h5>
+            </div>
+            <div>
+                <h5>Valor de la compra</h5>
+            </div>
         </div>
     </div>
     );
 }
 
 export default Pago;
+
